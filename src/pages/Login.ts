3cfdd2f0c220ff3d5cf5a8b098a9749629dc6290
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-07-10 18:37:18 +0200

    Add local storage functions to set and retrieve username

--- src/pages/Login.ts
+++ src/pages/Login.ts
@@ -1,1 +1,12 @@
+import { getRequiredElement } from "../utils/domHelpers.js";
+import { getUsername, setUsername } from "../utils/storage.js";
 
+function login(): void {
+  const usernameInput = getRequiredElement<HTMLInputElement>("#username");
+  const passwordInput = getRequiredElement<HTMLInputElement>("#password");
+
+  setUsername(usernameInput.value);
+}
+
+const loginForm = getRequiredElement<HTMLFormElement>("form");
+loginForm.addEventListener("submit", login);
