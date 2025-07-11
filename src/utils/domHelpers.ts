5314886e7b725a474ac2510e3240632316556ca0
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-07-11 14:25:26 +0200

    Extend getRequiredElement to query within specified element

--- src/utils/domHelpers.ts
+++ src/utils/domHelpers.ts
@@ -1,6 +1,14 @@
 // Fetch element and raise error if not found
-export function getRequiredElement<T extends HTMLElement>(selector: string): T {
-  const el = document.querySelector<T>(selector);
-  if (!el) throw new Error(`${selector} not found.`);
+export function getRequiredElement<T extends HTMLElement>(
+  selector: string,
+  root: Document | HTMLElement = document
+): T {
+  const el = root.querySelector<T>(selector);
+  if (!el)
+    throw new Error(
+      `${selector} not found in ${
+        root instanceof Document ? "document" : "element"
+      }.`
+    );
   return el;
 }
