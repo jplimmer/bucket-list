3ac69f607d01ed9bd3a1d61e91a656dcd7fb21c9
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-07-11 17:11:49 +0200

    Add storage methods for dreamList

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,7 +1,22 @@
-export function setUsername(username: string) {
+import { Dream } from "../models/types.js";
+
+export function setUsername(username: string): void {
   localStorage.setItem("username", username);
 }
 
 export function getUsername(): string | null {
   return localStorage.getItem("username");
+}
+
+export function saveDreamList(dreamList: Dream[]): void {
+  localStorage.setItem("dream-list", JSON.stringify(dreamList));
+}
+
+export function getDreamList(): Dream[] | null {
+  const storedList = localStorage.getItem("dream-list");
+  if (storedList) {
+    const dreams: Dream[] = JSON.parse(storedList);
+    return dreams;
+  }
+  return null;
 }
