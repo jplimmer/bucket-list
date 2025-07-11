7cf01571ecc6f3fcafcac3ce1717869c6438d73c
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-07-11 19:23:00 +0200

    Create separate file for mock variables

---
+++ src/constants/mockVariables.ts
@@ -0,0 +1,22 @@
+import { Dream } from "../models/types.js";
+
+export const dreams: Dream[] = [
+  {
+    id: 1,
+    name: "Lära mig HTML/CSS",
+    theme: "teknikdrömmar",
+    isChecked: true,
+  },
+  {
+    id: 2,
+    name: "Lära mig TypeScript",
+    theme: "teknikdrömmar",
+    isChecked: false,
+  },
+  {
+    id: 3,
+    name: "En dröm som tar flera rader lorem ipsum",
+    theme: "vardagsdrömmar",
+    isChecked: false,
+  },
+];
