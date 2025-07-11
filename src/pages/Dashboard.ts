c6728d62fea8e8afae98e45b437f9f1bc5836616
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-07-11 19:25:20 +0200

    Render dreamList on dashboard page (and remove hard-coded HTML mock variables)

--- src/pages/Dashboard.ts
+++ src/pages/Dashboard.ts
@@ -1,9 +1,24 @@
 import { getRequiredElement } from "../utils/domHelpers.js";
-import { getUsername } from "../utils/storage.js";
+import { getDreamList, getUsername, saveDreamList } from "../utils/storage.js";
+import { Dream } from "../models/types.js";
+import { renderList } from "../ui/renderList.js";
+import { createDreamListItem } from "../ui/dreamListItem.js";
 
-// Display username
-const nameSpan = getRequiredElement<HTMLSpanElement>("#user-name");
-const userName = getUsername();
-nameSpan.textContent = userName ? `, ${userName}` : "!";
+// Mock variables
+import { dreams } from "../constants/mockVariables.js";
+saveDreamList(dreams);
 
-// Render dream list
+document.addEventListener("DOMContentLoaded", () => {
+  // Display username
+  const nameSpan = getRequiredElement<HTMLSpanElement>("#user-name");
+  const userName = getUsername();
+  nameSpan.textContent = userName ? `, ${userName}` : "!";
+
+  // Render dream list
+  const dreamListContainer =
+    getRequiredElement<HTMLUListElement>(".dream-list");
+  const dreamList = getDreamList();
+  if (dreamList) {
+    renderList<Dream>(dreamListContainer, dreamList, createDreamListItem);
+  }
+});
--- src/ui/dreamListItem.ts
+++ src/ui/dreamListItem.ts
@@ -4,15 +4,29 @@ import { getRequiredElement } from "../utils/domHelpers.js";
 export function createDreamListItem(dream: Dream): HTMLLIElement {
   const tpl = getRequiredElement<HTMLTemplateElement>("#dream-tpl");
   const li = tpl.content.firstElementChild!.cloneNode(true) as HTMLLIElement;
+  li.setAttribute("role", "list");
 
-  const checkbox = getRequiredElement<HTMLInputElement>("input", li);
-  const label = getRequiredElement<HTMLLabelElement>("label", li);
-  const span = getRequiredElement<HTMLSpanElement>(".dream-theme", li);
-
-  checkbox.checked = dream.isChecked;
-  label.textContent = dream.name;
-  span.textContent = dream.theme;
   li.dataset.id = dream.id.toString();
+  const itemId = `dream-check-${dream.id.toString()}`;
+
+  // Checkbox
+  const checkbox = getRequiredElement<HTMLInputElement>("input", li);
+  checkbox.id = itemId;
+  checkbox.checked = dream.isChecked;
+
+  // Label
+  const label = getRequiredElement<HTMLLabelElement>("label", li);
+  label.htmlFor = itemId;
+  // Prepend to avoid overwriting child <span> element
+  label.prepend(dream.name);
+
+  // Dream-theme span
+  const span = getRequiredElement<HTMLSpanElement>(".dream-theme", li);
+  span.textContent = dream.theme;
+
+  // Delete button
+  const delBtn = getRequiredElement<HTMLButtonElement>("button", li);
+  delBtn.setAttribute("aria-label", `Ta bort '${dream.name}'`);
 
   return li;
 }
