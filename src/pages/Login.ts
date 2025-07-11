8040869f37d50a65cb8052899bb95d72a7151fcb
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-07-11 12:44:07 +0200

    Add input sanitisation util, username and password validation, and UI management for login

--- src/pages/Login.ts
+++ src/pages/Login.ts
@@ -1,12 +1,39 @@
 import { getRequiredElement } from "../utils/domHelpers.js";
-import { getUsername, setUsername } from "../utils/storage.js";
+import { login } from "../services/auth.js";
 
-function login(): void {
-  const usernameInput = getRequiredElement<HTMLInputElement>("#username");
-  const passwordInput = getRequiredElement<HTMLInputElement>("#password");
-
-  setUsername(usernameInput.value);
-}
-
+// Find DOM elements
 const loginForm = getRequiredElement<HTMLFormElement>("form");
-loginForm.addEventListener("submit", login);
+const usernameInput = getRequiredElement<HTMLInputElement>("#username");
+const passwordInput = getRequiredElement<HTMLInputElement>("#password");
+const usernameError = getRequiredElement<HTMLParagraphElement>(
+  "#username-error-message"
+);
+const passwordError = getRequiredElement<HTMLParagraphElement>(
+  "#password-error-message"
+);
+
+// Call login on form submit, display errors if unsuccessful
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const result = await login(usernameInput.value, passwordInput.value);
+
+  if (!result.success) {
+    // Update and display error messages
+    usernameError.textContent = result.errors.username || "";
+    passwordError.textContent = result.errors.password || "";
+    usernameError.classList.remove("hidden");
+    passwordError.classList.remove("hidden");
+    // If username error, replace user input with suggested valid input
+    if (result.errors.username) {
+      usernameInput.value = result.suggestion ?? "";
+    }
+  } else {
+    // Hide error messages
+    usernameError.classList.add("hidden");
+    passwordError.classList.add("hidden");
+
+    // Redirect to dashboard
+    window.location.href = "dashboard.html";
+  }
+});
