c35b86cd57ade48ed201e747b6e19eb722b99446
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-07-15 17:36:16 +0200

    Add storage type to STORAGE_CONFIG

--- src/constants/storageConfig.ts
+++ src/constants/storageConfig.ts
@@ -1,7 +1,17 @@
-/** Local storage keys for persisting user data. */
-export const STORAGE_CONFIG = {
+type StorageType = typeof localStorage | typeof sessionStorage;
+
+interface StorageConfig {
+  /** Type of storage to use for saving data. */
+  STORAGE_TYPE: StorageType;
   /** Storage key for username. */
-  USERNAME: "username",
+  USERNAME: string;
   /** Storage key for dream list. */
+  DREAM_LIST: string;
+}
+
+/** Local storage keys for persisting user data. */
+export const STORAGE_CONFIG: StorageConfig = {
+  STORAGE_TYPE: localStorage,
+  USERNAME: "username",
   DREAM_LIST: "dream-list",
 } as const;
